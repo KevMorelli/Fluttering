1287dd4747b8d5c78278385b3d33059f91be11ea
Kevin Morelli <kevin.morelli@dgenix.com>
2019-05-20 15:40:56 -0300

    Fix vertical overflow

--- star_wars/lib/views/planet/components/body.dart
+++ star_wars/lib/views/planet/components/body.dart
@@ -8,119 +8,121 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     final planetProvider = Provider.of<PlanetProvider>(context);
 
-    return Center(
-      child: Column(
-        children: <Widget>[
-          Container(
-            constraints: new BoxConstraints.expand(
-              height: 280.0,
-            ),
-            alignment: Alignment.bottomLeft,
-            padding: new EdgeInsets.only(left: 16.0, bottom: 8.0),
-            decoration: new BoxDecoration(
-              image: new DecorationImage(
-                image: new AssetImage('assets/planet_header.png'),
-                fit: BoxFit.cover,
+    return SingleChildScrollView(
+      child: Center(
+        child: Column(
+          children: <Widget>[
+            Container(
+              constraints: new BoxConstraints.expand(
+                height: 280.0,
               ),
-            ),
-            child: new Text(planetProvider.getPlanet().name,
-                style: new TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 36.0,
-                    color: Color(0xffff0000))),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-                left: 25.0, top: 20.0, bottom: 20.0, right: 25.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text('Climate'),
-                Text(planetProvider.getPlanet().climate)
-              ],
-            ),
-          ),
-          Container(
-            color: Color(0xff2c2c2c),
-            height: 3.0,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-                left: 25.0, top: 20.0, bottom: 20.0, right: 25.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text('Diameter'),
-                Text(planetProvider.getPlanet().diameter)
-              ],
-            ),
-          ),
-          Container(
-            color: Color(0xff2c2c2c),
-            height: 3.0,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-                left: 25.0, top: 20.0, bottom: 20.0, right: 25.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text('Gravity'),
-                Text(planetProvider.getPlanet().gravity)
-              ],
-            ),
-          ),
-          Container(
-            color: Color(0xff2c2c2c),
-            height: 3.0,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-                left: 25.0, top: 20.0, bottom: 20.0, right: 25.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text('Terrain'),
-                Text(planetProvider.getPlanet().terrain)
-              ],
-            ),
-          ),
-          Container(
-            color: Color(0xff2c2c2c),
-            height: 3.0,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-                left: 25.0, top: 20.0, bottom: 20.0, right: 25.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text('Population'),
-                Text(planetProvider.getPlanet().population)
-              ],
-            ),
-          ),
-          Container(
-            color: Color(0xff2c2c2c),
-            height: 3.0,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: SizedBox(
-              height: 65.0,
-              width: 200.0,
-              child: RaisedButton(
-                onPressed: () {},
-                child: Text(
-                  'DESTROY',
-                  style: TextStyle(fontSize: 18.0),
+              alignment: Alignment.bottomLeft,
+              padding: new EdgeInsets.only(left: 16.0, bottom: 8.0),
+              decoration: new BoxDecoration(
+                image: new DecorationImage(
+                  image: new AssetImage('assets/planet_header.png'),
+                  fit: BoxFit.cover,
                 ),
-                color: Color(0xffff0000),
-                textColor: Colors.white,
+              ),
+              child: new Text(planetProvider.getPlanet().name,
+                  style: new TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 36.0,
+                      color: Color(0xffff0000))),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 25.0, top: 20.0, bottom: 20.0, right: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text('Climate'),
+                  Text(planetProvider.getPlanet().climate)
+                ],
               ),
             ),
-          ),
-        ],
+            Container(
+              color: Color(0xff2c2c2c),
+              height: 3.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 25.0, top: 20.0, bottom: 20.0, right: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text('Diameter'),
+                  Text(planetProvider.getPlanet().diameter)
+                ],
+              ),
+            ),
+            Container(
+              color: Color(0xff2c2c2c),
+              height: 3.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 25.0, top: 20.0, bottom: 20.0, right: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text('Gravity'),
+                  Text(planetProvider.getPlanet().gravity)
+                ],
+              ),
+            ),
+            Container(
+              color: Color(0xff2c2c2c),
+              height: 3.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 25.0, top: 20.0, bottom: 20.0, right: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text('Terrain'),
+                  Text(planetProvider.getPlanet().terrain)
+                ],
+              ),
+            ),
+            Container(
+              color: Color(0xff2c2c2c),
+              height: 3.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 25.0, top: 20.0, bottom: 20.0, right: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text('Population'),
+                  Text(planetProvider.getPlanet().population)
+                ],
+              ),
+            ),
+            Container(
+              color: Color(0xff2c2c2c),
+              height: 3.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: SizedBox(
+                height: 65.0,
+                width: 200.0,
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'DESTROY',
+                    style: TextStyle(fontSize: 18.0),
+                  ),
+                  color: Color(0xffff0000),
+                  textColor: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
