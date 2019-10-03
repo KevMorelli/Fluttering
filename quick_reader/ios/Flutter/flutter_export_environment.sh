685407efb48fe78ed976c2befe29cedb363100d0
Kevin Morelli <kev9318@gmail.com>
2019-10-03 00:39:42 -0300

    Update quick reader files

---
+++ quick_reader/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/Kev/Development/SDK/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/Kev/Development/Github/Fluttering/quick_reader"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/Kev/Development/SDK/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
